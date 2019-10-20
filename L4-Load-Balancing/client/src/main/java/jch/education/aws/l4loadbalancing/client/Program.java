bbf83c8d51c2e0b9e0b7dd50211da73936da49b9
Jaroslav Chmurny <jaroslav.chmurny@gmail.com>
2019-10-20 11:34:33 +0200

    Initial setup (pom.xml files, dummy main class for client and server).

---
+++ L4-Load-Balancing/client/src/main/java/jch/education/aws/l4loadbalancing/client/Program.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2019 Jaroslav Chmurny
+ *
+ * This file is part of AWS Sandbox.
+ *
+ * AWS Sandbox is free software developed for educational purposes. It
+ * is licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jch.education.aws.l4loadbalancing.client;
+
+public class Program {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello world!!! My name is Client.");
+    }
+}
