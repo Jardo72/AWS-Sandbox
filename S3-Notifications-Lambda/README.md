3400e508fb21059be2e2b764de88fa9a2f04f122
Jaroslav Chmurny <jaroslav.chmurny@gmail.com>
2021-08-17 21:12:50 +0200

    Initial commit - CloudFormation template creates almost complete stack, handler function successfully invoked if the notification is configured manually.

---
+++ S3-Notifications-Lambda/README.md
@@ -0,0 +1,26 @@
+# 
+
+## Introduction
+- Lambda function used as event handler for S3 event notifications
+- files uploaded to an S3 bucket representing results of ice hockey games
+- upload of such a file triggers the Lambda function which parses the game results, calculates standings based on the parsed game results, and sends the calculated standings to an SQS queue
+
+## Source Code Organization and Build
+- Maven multi-module
+- library
+  + completely independent of AWS SDK
+  + provides functionality like parsing of game results, calculation and generation of standings
+- Lambda function just integrates the functionality provided by the library into the Lambda environment and with the AWS services used
+
+## Deployment
+
+```
+aws cloudformation create-stack --stack-name Lambda-Ice-Hockey --template-body file://cloud-formation-template.yml --parameters file://stack-params.json --capabilities CAPABILITY_NAMED_IAM --on-failure ROLLBACK
+```
+
+TODO:
+- there is no notification configuration - this must be created manually
+- it seems that this cannot be done directly via CloudFormation - there seems to be a problem with cyclic dependencies
+- however, it seems that CloudFormation can create a Lambda function that will configure the S3 event notification
+- see https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-s3-bucket-notificationconfig.html
+- see https://aws.amazon.com/premiumsupport/knowledge-center/cloudformation-s3-notification-lambda/
---
+++ S3-Notifications-Lambda/model.py
@@ -0,0 +1,95 @@
+#
+# Copyright 2021 Jaroslav Chmurny
+#
+# This file is part of AWS Sandbox.
+#
+# AWS Sandbox is free software developed for educational purposes. It
+# is licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicationlicationlicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from dataclasses import dataclass
+from enum import Enum, unique
+
+
+@unique
+class ResultType(Enum):
+    REGULATION = 1
+    OVERTIME = 2
+    SHOOTOUT = 3
+
+
+@dataclass(frozen=True)
+class SingleGameTeamRecord:
+    team: str
+    goals: int
+
+
+@dataclass(frozen=True)
+class GameResult:
+    home_team_record: SingleGameTeamRecord
+    visitor_team_record: SingleGameTeamRecord
+    type: ResultType
+
+    @property
+    def home_team(self) -> str:
+        return self.home_team_record.team
+
+    @property
+    def home_team_goals(self) -> int:
+        return self.home_team_record.goals
+
+    @property
+    def visitor_team(self) -> str:
+        return self.visitor_team.team
+
+    @property
+    def visitor_team_goals(self) -> int:
+        return self.visitor_team.goals
+
+
+@dataclass(frozen=True)
+class GameStatistics:
+    regulation_win_count: int
+    overtime_win_count: int
+    overtime_loss_count: int
+    regulation_loss_count: int
+
+    @property
+    def overall_game_count(self) -> int:
+        return self.regulation_win_count + self.overtime_win_count + self.regulation_loss_count + slef.overtime_loss_count
+
+
+@dataclass(frozen=True)
+class Score:
+    goals_for: int
+    goals_against: int
+
+
+@dataclass(frozen=True)
+class StandingsEntry:
+    team: str
+    game_statistics: GameStatistics
+    score: Score
+    points: int
+
+    @property
+    def overall_game_count(self) -> int:
+        return self.game_statistics.overall_game_count
+
+    @property
+    def goals_for(self) -> int:
+        return self.score.goals_for
+
+    @property
+    def goals_against(self) -> int:
+        return self.score.goals_against
