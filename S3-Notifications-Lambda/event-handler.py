3fb3ef46d8b9628bfc14886d86aa8412a894c1be
Jaroslav Chmurny <jaroslav.chmurny@gmail.com>
2021-08-21 22:17:34 +0200

    First implementation steps for the Lambda function.

--- S3-Notifications-Lambda/event-handler.py
+++ S3-Notifications-Lambda/event-handler.py
@@ -17,10 +17,56 @@
 # limitations under the License.
 #
 
-import model
-import standings
 
+from boto3 import client
+from os import environ
+
+from model import ResultType, StandingsEntry
+from output import print_standings
+from standings import Configuration
+
+
+class LambdaConfiguration(Configuration):
+
+    def __init__(self) -> None:
+        self._regulation_win_points = int(environ['REGULATION_WIN_POINTS'])
+        self._overtime_win_points = int(environ['OVERTIME_WIN_POINTS'])
+        self._shootout_win_points = int(environ['SHOOTOUT_WIN_POINTS'])
+        self._regulation_loss_points = int(environ['REGULATION_LOSS_POINTS'])
+        self._overtime_loss_points = int(environ['OVERTIME_LOSS_POINTS'])
+        self._shootout_loss_points = int(environ['SHOOTOUT_LOSS_POINTS'])
+
+    def get_points_for_win(self, result_type: ResultType) -> int:
+        if result_type == ResultType.REGULATION:
+            return self._regulation_win_points
+        elif result_type == ResultType.OVERTIME:
+            return self._overtime_win_points
+        elif result_type == ResultType.SHOOTOUT:
+            return self._shootout_win_points
+        raise ValueError(f'Unexpected result type: {result_type}.')
+
+    def get_points_for_loss(self, result_type: ResultType) -> int:
+        if result_type == ResultType.REGULATION:
+            return self._regulation_loss_points
+        elif result_type == ResultType.OVERTIME:
+            return self._overtime_loss_points
+        elif result_type == ResultType.SHOOTOUT:
+            return self._shootout_loss_points
+        raise ValueError(f'Unexpected result type: {result_type}.')
+
+
+def process_single_file(record):
+    bucket = record['s3']['bucket']['name']
+    filename = record['s3']['object']['key']
+    print(f'Processing file: bucket = "{bucket}", filename = "{filename}"')
+
+# https://stackoverflow.com/questions/46928105/reading-files-triggered-by-s3-event
 def main(event, context):
     print('S3 notification event handler invoked, look at the event')
     print(event)
+
+    configuration = LambdaConfiguration()
+    # s3_client = client('s3')
+    for record in event['Records']:
+        process_single_file(record)
     return 'Done :-)'
