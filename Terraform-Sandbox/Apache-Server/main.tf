f9000be61eaa500148cc7bb7dd01c76e8a439d7f
Jaroslav Chmurny <38262696+Jardo72@users.noreply.github.com>
2022-05-03 21:42:46 +0200

    First Terraform provisioning is now working (VPC, IGW, subnet, route table, security group, EC2 instance)

--- Terraform-Sandbox/Apache-Server/main.tf
+++ Terraform-Sandbox/Apache-Server/main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_vpc" "ApacheVPC" {
     cidr_block = "10.0.0.0/16"
     instance_tenancy = "default"
+    enable_dns_hostnames = true
     tags = {
         Name = "Apache-VPC"
     }
@@ -26,7 +27,7 @@ resource "aws_subnet" "ApachePublicSubnet" {
 resource "aws_route_table" "PublicRouteTable" {
     vpc_id = aws_vpc.ApacheVPC.id
     route {
-        cidr_block = 0.0.0.0/0
+        cidr_block = "0.0.0.0/0"
         gateway_id = aws_internet_gateway.ApacheIGV.id
     }
     tags = {
@@ -39,6 +40,45 @@ resource "aws_route_table_association" "PublicRouteTableAssociation" {
     route_table_id = aws_route_table.PublicRouteTable.id
 }
 
-# resource "aws_instance" "WebServer" {
-#
-# }
+resource "aws_security_group" "WebServerSG" {
+    name = "Web-Server-SG"
+    description = "Allow inbound HTTP traffic from any origin"
+    vpc_id = aws_vpc.ApacheVPC.id
+
+    ingress {
+        protocol = "tcp"
+        from_port = 80
+        to_port = 80
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    egress {
+        protocol = "-1"
+        from_port = 0
+        to_port = 0
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags = {
+        Name = "Web-Server-SG"
+    }
+}
+
+resource "aws_instance" "WebServer" {
+    ami = "ami-09439f09c55136ecf"
+    instance_type = "t2.nano"
+    subnet_id = aws_subnet.ApachePublicSubnet.id
+    vpc_security_group_ids = [aws_security_group.WebServerSG.id]
+    user_data = <<EOF
+#!/bin/bash
+yum update -y
+yum install httpd -y
+service httpd start
+chkconfig httpd on
+EOF
+    tags = {
+        Name = "WebServer"
+    }
+}
