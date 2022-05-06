ee6adc1f81c237d0e260fddba92936484afc16fe
Jaroslav Chmurny <jaroslav.chmurny@gmail.com>
2022-05-06 12:41:55 +0200

    New features added (EC2 instance profile, random password stored in SSM parameter store, AMI ID from SSM parameter store etc.)

--- Terraform-Sandbox/Apache-Server/main.tf
+++ Terraform-Sandbox/Apache-Server/main.tf
@@ -2,48 +2,70 @@ provider "aws" {
     region = "eu-central-1"
 }
 
-resource "aws_vpc" "ApacheVPC" {
+data "aws_caller_identity" "current" {}
+
+data "aws_ssm_parameter" "CurrentVersionAMIID" {
+    name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
+
+locals {
+    ssm_parameter_name = "/terraform-sandbox/apache-server/dummy-password"
+    common_tags = {
+        Name = "Apache-Demo",
+        ProvisionedBy = "Terraform"
+    }
+}
+
+resource "random_password" "DummyPassword" {
+    length = 12
+    special = true
+}
+
+resource "aws_ssm_parameter" "DummyPassword" {
+    description = "Dummy password for no real use"
+    type = "SecureString"
+    name = local.ssm_parameter_name
+    value = random_password.DummyPassword.result
+    tags = local.common_tags
+}
+
+resource "aws_vpc" "VPC" {
     cidr_block = "10.0.0.0/16"
     instance_tenancy = "default"
     enable_dns_hostnames = true
-    tags = {
-        Name = "Apache-VPC"
-    }
+    tags = local.common_tags
 }
 
-resource "aws_internet_gateway" "ApacheIGV" {
-    vpc_id = aws_vpc.ApacheVPC.id
+resource "aws_internet_gateway" "IGW" {
+    vpc_id = aws_vpc.VPC.id
+    tags = local.common_tags
 }
 
-resource "aws_subnet" "ApachePublicSubnet" {
-    vpc_id = aws_vpc.ApacheVPC.id
+resource "aws_subnet" "PublicSubnet" {
+    vpc_id = aws_vpc.VPC.id
     cidr_block = "10.0.0.0/24"
     map_public_ip_on_launch = true
-    tags = {
-        Name = "Apache-Public-Subnet"
-    }
+    tags = local.common_tags
 }
 
 resource "aws_route_table" "PublicRouteTable" {
-    vpc_id = aws_vpc.ApacheVPC.id
+    vpc_id = aws_vpc.VPC.id
     route {
         cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.ApacheIGV.id
+        gateway_id = aws_internet_gateway.IGW.id
     }
-    tags = {
-        Name = "Apache-Public-Route-Table"
-    }
+    tags = local.common_tags
 }
 
 resource "aws_route_table_association" "PublicRouteTableAssociation" {
-    subnet_id = aws_subnet.ApachePublicSubnet.id
+    subnet_id = aws_subnet.PublicSubnet.id
     route_table_id = aws_route_table.PublicRouteTable.id
 }
 
 resource "aws_security_group" "WebServerSG" {
-    name = "Web-Server-SG"
+    name = "Apache-Demo"
     description = "Allow inbound HTTP(S) traffic from any origin"
-    vpc_id = aws_vpc.ApacheVPC.id
+    vpc_id = aws_vpc.VPC.id
 
     dynamic "ingress" {
         for_each = ["80", "443"]
@@ -65,26 +87,83 @@ resource "aws_security_group" "WebServerSG" {
         ipv6_cidr_blocks = ["::/0"]
     }
 
-    tags = {
-        Name = "Web-Server-SG"
-    }
+    tags = local.common_tags
 }
 
 resource "aws_eip" "WebServerElasticIP" {
     instance = aws_instance.WebServer.id
     vpc = true
+    tags = local.common_tags
+}
+
+resource "aws_iam_role" "SSMParameterReaderRole" {
+    name = "SSMParameterReaderRole"
+    assume_role_policy = jsonencode({
+        Version: "2012-10-17",
+        Statement: [
+            {
+                Sid: "AllowEC2Assume",
+                Action: "sts:AssumeRole",
+                Principal: {
+                    Service: "ec2.amazonaws.com"
+                },
+                Effect: "Allow"
+            }
+        ]
+    })
+
+    inline_policy {
+        name = "SSMParameterValueReadAccess"
+        policy = jsonencode({
+            Version: "2012-10-17",
+            Statement: [
+                {
+                    Sid: "AllowGetParameter",
+                    Action: ["ssm:GetParameter"]
+                    Effect: "Allow"
+                    Resource: format("arn:aws:ssm:eu-central-1:%s:parameter%s", data.aws_caller_identity.current.account_id, local.ssm_parameter_name)
+                }
+            ]
+        })
+    }
+
+    tags = local.common_tags
+}
+
+resource "aws_iam_instance_profile" "SSMParameterReaderProfile" {
+    name = "SSMParameterReaderProfile"
+    role = aws_iam_role.SSMParameterReaderRole.name
+    tags = local.common_tags
 }
 
 resource "aws_instance" "WebServer" {
-    ami = "ami-09439f09c55136ecf"
+    ami = data.aws_ssm_parameter.CurrentVersionAMIID.value
     instance_type = "t2.nano"
-    subnet_id = aws_subnet.ApachePublicSubnet.id
+    subnet_id = aws_subnet.PublicSubnet.id
     vpc_security_group_ids = [aws_security_group.WebServerSG.id]
-    user_data = file("user-data.sh")
-    tags = {
-        Name = "WebServer"
-    }
+    iam_instance_profile = aws_iam_instance_profile.SSMParameterReaderProfile.name
+    user_data = templatefile("user-data.tpl", {
+        ssm_parameter_name = local.ssm_parameter_name
+    })
+    tags = local.common_tags
     lifecycle {
         create_before_destroy = true
     }
+    depends_on = [aws_ssm_parameter.DummyPassword]
+}
+
+output "AWSAccountID" {
+    value = data.aws_caller_identity.current.account_id
+}
+
+output "CurrentVersionAMIID" {
+    value = nonsensitive(data.aws_ssm_parameter.CurrentVersionAMIID.value)
+}
+
+output "WebServerElasticIP" {
+    value = aws_eip.WebServerElasticIP.public_ip
+}
+
+output "WebServerElasticDNSName" {
+    value = aws_eip.WebServerElasticIP.public_dns
 }
---
+++ Terraform-Sandbox/Apache-Server/user-data.tpl
@@ -0,0 +1,37 @@
+#!/bin/bash
+yum update -y
+yum install httpd -y
+yum install amazon-cloudwatch-agent -y
+
+LOCAL_IP_ADDRESS=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+HOSTNAME=`curl http://169.254.169.254/latest/meta-data/hostname`
+INSTANCE_TYPE=`curl http://169.254.169.254/latest/meta-data/instance-type`
+INSTANCE_ID=`curl http://169.254.169.254/latest/meta-data/instance-id`
+START_TIME=`date '+%Y-%m-%d %H:%M:%S'`
+SSM_PARAM_DETAILS=`aws ssm get-parameter --name ${ssm_parameter_name} --with-decryption`
+
+cat > /var/www/html/index.html <<HTML_EOF
+<html>
+<head>
+<title>EC2 Apache Server</title>
+</head>
+<body>
+<h2>EC2 Apache Server</h2>
+<pre>
+Hostname         = $HOSTNAME
+Local IP address = $LOCAL_IP_ADDRESS
+Instance type    = $INSTANCE_TYPE
+Instance ID      = $INSTANCE_ID
+Start time       = $START_TIME
+SSM param. name  = ${ssm_parameter_name}
+
+SSM Parameter details:
+$SSM_PARAM_DETAILS
+
+</pre>
+</body>
+</html>
+HTML_EOF
+
+systemctl start httpd
+systemctl enable httpd
