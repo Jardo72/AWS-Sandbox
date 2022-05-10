d19f39614b94234a27f70f85ec78abf985547901
Jaroslav Chmurny <jaroslav.chmurny@gmail.com>
2022-05-10 15:37:35 +0200

    Private subnets with their own route table douting to a NAT gateway have been added.

--- Terraform-Sandbox/L7-Load-Balancing/main.tf
+++ Terraform-Sandbox/L7-Load-Balancing/main.tf
@@ -12,7 +12,7 @@ locals {
 data "aws_availability_zones" "available" {}
 
 resource "aws_vpc" "vpc" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = var.vpc_cidr_block
   instance_tenancy     = "default"
   enable_dns_hostnames = true
   enable_dns_support   = true
@@ -40,6 +40,22 @@ resource "aws_subnet" "public_subnet_two" {
   tags                    = local.common_tags
 }
 
+resource "aws_subnet" "private_subnet_one" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  cidr_block              = "10.0.10.0/24"
+  map_public_ip_on_launch = false
+  tags                    = local.common_tags
+}
+
+resource "aws_subnet" "private_subnet_two" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = data.aws_availability_zones.available.names[1]
+  cidr_block              = "10.0.11.0/24"
+  map_public_ip_on_launch = false
+  tags                    = local.common_tags
+}
+
 resource "aws_eip" "nat_gateway_elastic_ip" {
   vpc  = true
   tags = local.common_tags
@@ -48,8 +64,8 @@ resource "aws_eip" "nat_gateway_elastic_ip" {
 # https://dev.betterdoc.org/infrastructure/2020/02/04/setting-up-a-nat-gateway-on-aws-using-terraform.html
 resource "aws_nat_gateway" "nat_gateway" {
   allocation_id = aws_eip.nat_gateway_elastic_ip.id
-  subnet_id = aws_subnet.public_subnet_one.id
-  tags = local.common_tags
+  subnet_id     = aws_subnet.public_subnet_one.id
+  tags          = local.common_tags
 }
 
 resource "aws_route_table" "public_subnets_route_table" {
@@ -69,4 +85,23 @@ resource "aws_route_table_association" "public_subnet_one_route_table_associatio
 resource "aws_route_table_association" "public_subnet_two_route_table_association" {
   subnet_id      = aws_subnet.public_subnet_two.id
   route_table_id = aws_route_table.public_subnets_route_table.id
+}
+
+resource "aws_route_table" "private_subnets_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+  tags = local.common_tags
+}
+
+resource "aws_route_table_association" "private_subnet_one_route_table_association" {
+  subnet_id      = aws_subnet.private_subnet_one.id
+  route_table_id = aws_route_table.private_subnets_route_table.id
+}
+
+resource "aws_route_table_association" "private_subnet_two_route_table_association" {
+  subnet_id      = aws_subnet.private_subnet_two.id
+  route_table_id = aws_route_table.private_subnets_route_table.id
 }
--- Terraform-Sandbox/L7-Load-Balancing/variables.tf
+++ Terraform-Sandbox/L7-Load-Balancing/variables.tf
@@ -4,6 +4,12 @@ variable "aws_region" {
   default     = "eu-central-1"
 }
 
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC (default = 10.0.0.0/16)"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
 variable "ec2_instance_type" {
   description = "Instance type for the EC2 instances running the L7 Load-Balancing Demo application"
   type        = string
