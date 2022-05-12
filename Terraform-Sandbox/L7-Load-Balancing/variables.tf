0b760f2cb2762ecb2118d9d2a04bd1ea6a7ef8dc
Jaroslav Chmurny <jaroslav.chmurny@gmail.com>
2022-05-12 21:48:19 +0200

    Further steps towards load-balancer & auto-scaling.

--- Terraform-Sandbox/L7-Load-Balancing/variables.tf
+++ Terraform-Sandbox/L7-Load-Balancing/variables.tf
@@ -1,3 +1,22 @@
+#
+# Copyright 2022 Jaroslav Chmurny
+#
+# This file is part of AWS Sandbox.
+#
+# AWS Sandbox is free software developed for educational purposes. It
+# is licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 variable "aws_region" {
   description = "The AWS region where the resources are to be provisioned"
   type        = string
@@ -26,4 +45,10 @@ variable "ec2_port" {
   description = "TCP port the application instances running on EC2 will use to accept incoming connections"
   type        = number
   default     = 80
+}
+
+variable "target_cpu_utilization_threshold" {
+  description = "Threshold for the aggregate CPU utilization for the autoscaling group; if the CPU utilization will exceed this value, autoscaling will scale out the autoscaling group"
+  type        = number
+  default     = 50
 }
