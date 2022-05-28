85e833e6b4582518b94d02dd460ccd5670228466
chmurny <Chmurny.Jaroslav-ext@komix.cz>
2022-05-28 22:37:11 +0200

    First step towards module.

---
+++ Terraform-Sandbox/Modules/variables.tf
@@ -0,0 +1,43 @@
+#
+# Copyright 2022 Jaroslav Chmurny
+#
+# This file is part of AWS Sandbox.
+#
+# AWS Sandbox is free software developed for educational purposes. It
+# is licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  description = "Definition of subnets for particular availability zones"
+  type = map(object({
+    az_name                   = string
+    public_subnet_cidr_block  = string
+    private_subnet_cidr_block = string
+  }))
+}
+
+variable "resource_name_prefix" {
+  description = "Prefix for the names to be applied to the provisioned resources"
+  type        = string
+}
+
+variable "tags" {
+  description = "Common tags to be applied to the provisioned resources"
+  type        = map(string)
+}
