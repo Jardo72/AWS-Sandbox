d31f58581dff8e9fed9f2877bb2218cb7b882732
chmurny <Chmurny.Jaroslav-ext@komix.cz>
2022-06-03 10:41:43 +0200

    Fine tuning of Terraform configuration, plus fine tuning of the CloudWatch dashboard.

--- Terraform-Sandbox/L7-Load-Balancing/main.tf
+++ Terraform-Sandbox/L7-Load-Balancing/main.tf
@@ -17,6 +17,16 @@
 # limitations under the License.
 #
 
+terraform {
+  required_version = ">=1.1"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~>4.15.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
