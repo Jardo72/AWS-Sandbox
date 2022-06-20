f242bf9efc8a3ff0bee91046008ce5f41615b105
chmurny <Chmurny.Jaroslav-ext@komix.cz>
2022-06-20 22:39:48 +0200

    Terraform configuration added to L4-LB-Demo.

--- Terraform-Sandbox/L4-Load-Balancing/main.tf
+++ Terraform-Sandbox/L4-Load-Balancing/main.tf
@@ -17,6 +17,20 @@
 # limitations under the License.
 #
 
+terraform {
+  required_version = ">=1.1"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>4.15.0"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = "~>2.2.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
