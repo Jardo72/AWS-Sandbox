ffe4fef53ca5a81cb5293aec1d2e7d9a5e106573
chmurny <Chmurny.Jaroslav-ext@komix.cz>
2022-06-24 16:30:49 +0200

    Tags for the ASG for L7-LB-Demo finalized, this was the last TODO in the code.

--- Terraform-Sandbox/L7-Load-Balancing/modules/asg/main.tf
+++ Terraform-Sandbox/L7-Load-Balancing/modules/asg/main.tf
@@ -129,8 +129,19 @@ resource "aws_autoscaling_group" "autoscaling_group" {
     version = "$Latest"
   }
   target_group_arns = [var.target_group_arn]
-  # TODO: make it work, or remove it
-  # tag = var.tags
+  tag {
+    key                 = "Name"
+    value               = "${var.resource_name_prefix}-ASG"
+    propagate_at_launch = false
+  }
+  dynamic "tag" {
+    for_each = var.tags
+    content {
+      key                 = tag.key
+      value               = tag.value
+      propagate_at_launch = true
+    }
+  }
 }
 
 resource "aws_autoscaling_policy" "scaling_policy" {
