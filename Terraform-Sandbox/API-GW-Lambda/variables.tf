ee7d785515e75424a41e6991e80c0457833615e8
chmurny <Chmurny.Jaroslav-ext@komix.cz>
2022-09-01 21:22:41 +0200

    Initial commit for API-GW-Lambda.

---
+++ Terraform-Sandbox/API-GW-Lambda/variables.tf
@@ -0,0 +1,42 @@
+#
+# Copyright 2022 Jaroslav Chmurny
+#
+# This file is part of AWS Sandbox.
+#
+# AWS Sandbox is free software developed for educational purposes. It
+# is licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+variable "aws_region" {
+  description = "The AWS region where the resources are to be provisioned"
+  type        = string
+}
+
+variable "route53_alias_settings" {
+  description = "Settings for the Route 53 alias for the ALB"
+  type = object({
+    enabled                = bool
+    alias_hosted_zone_name = string
+    alias_fqdn             = string
+  })
+}
+
+variable "resource_name_prefix" {
+  description = "Prefix for the names to be applied to the provisioned resources"
+  type        = string
+}
+
+variable "tags" {
+  description = "Common tags to be applied to the provisioned resources"
+  type        = map(string)
+}
