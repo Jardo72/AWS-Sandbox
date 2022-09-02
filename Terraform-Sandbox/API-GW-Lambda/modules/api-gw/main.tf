24fdd487e490d47170d38ed07f641177e21675f0
chmurny <Chmurny.Jaroslav-ext@komix.cz>
2022-09-02 20:46:54 +0200

    First steps towards API GW deployment & stage.

--- Terraform-Sandbox/API-GW-Lambda/modules/api-gw/main.tf
+++ Terraform-Sandbox/API-GW-Lambda/modules/api-gw/main.tf
@@ -24,3 +24,14 @@ resource "aws_api_gateway_rest_api" "rest_api" {
   }
   body = templatefile("${path.module}/api-definition.yml", {})
 }
+
+/* TODO:
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+}
+
+resource "aws_api_gateway_stage" "stage" {
+  deployment_id = aws_api_gateway_deployment.deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  stage_name    = "sandbox"
+} */
