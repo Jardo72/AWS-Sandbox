b0402295e9097027c12b1954df967a0ac4d5163d
chmurny <Chmurny.Jaroslav-ext@komix.cz>
2023-06-30 11:07:23 +0200

    ACL removed for S3 buckets.

--- Terraform-Sandbox/S3-Cross-Region-Replication/modules/s3_bucket/main.tf
+++ Terraform-Sandbox/S3-Cross-Region-Replication/modules/s3_bucket/main.tf
@@ -31,11 +31,6 @@ resource "aws_s3_bucket" "bucket" {
   tags   = var.tags
 }
 
-resource "aws_s3_bucket_acl" "bucket_acl" {
-  bucket = aws_s3_bucket.bucket.id
-  acl    = "private"
-}
-
 resource "aws_s3_bucket_versioning" "versioning" {
   bucket = aws_s3_bucket.bucket.id
   versioning_configuration {
